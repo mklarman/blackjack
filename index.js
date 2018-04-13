10f1356382356842f015fda3c9649375ab5a333a
mklarman <mklarman@hotmail.com>
2018-04-13 10:40:46 -0400

    working on helper functions

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
+// Gotta create a players object.  This object will hold a name and a score.  Object
+// is made with a function that loops through an argument and for each i we create the player object and push it to an array.
+// Need functions for: creating a deck, shuffling a deck, dealing cards, summing up hand totals,
+// hitting, staying, updating points, declaring a winner.  Restart the game using the same deck.
+
+
 var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 var suits = ["diamonds", "hearts", "spades", "clubs"];
 var deck = []
+var players = []
+
+function createPlayers(num){
+	hand = []
+	for(i=0; i<=num; i++){
+		var player = {name: "player" + i, hand: hand, score: 0}
+		players.push(player)
+		if(player.name == "player0"){
+			player.name = "Dealer"
+		}
+		if(player.name == "player1"){
+			player.name = "Me"
+		}
+	}
+	return players
+}
+
 
 function getDeck(){
 
-	for(var i = 0; i < suits.length; i++)
-	{
-		for(var x = 0; x < cards.length; x++)
-		{
-			var card = {Value: cards[x], Suit: suits[i]};
-			deck.push(card);
+	for(var i = 0; i < suits.length; i++){
+		
+		for(var x = 0; x < cards.length; x++){
+			 
+			 var weight = parseInt(cards[x]);
+              if (cards[x] == "J" || cards[x] == "Q" || cards[x] == "K")
+                      weight = 10;
+              if (cards[x] == "A")
+                     weight = 11;
+              var card = { Value: cards[x], Suit: suits[i], Weight: weight };
+              deck.push(card);
 		}
 	}
 
@@ -17,7 +45,6 @@ function getDeck(){
 }
 
 function deal(){
-	console.log("hello")
 	// remove top card from deck
 	var card = deck[deck.length-1];
 	deck.splice(deck.length-1, 1);
@@ -55,3 +82,22 @@ function renderDeck(){
 		document.getElementById("deck").appendChild(card);
 	}
 }
+
+function dealHands()
+    {
+        // alternate handing cards to each player
+        // 2 cards each
+        for(var i = 0; i < 2; i++)
+        {
+            for (var x = 0; x < players.length; x++)
+            {
+                var card = deck.pop();
+                players[x].Hand.push(card);
+                renderCard(card, x);
+                updatePoints();
+            }
+        }
+
+        updateDeck();
+    }
+
