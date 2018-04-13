db3c3e0d007ff54e52a195ac0ec6f8cc8c396cd9
mklarman <mklarman@hotmail.com>
2018-04-12 23:23:03 -0400

    my deck

---
+++ index.js
@@ -0,0 +1,57 @@
+var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var suits = ["diamonds", "hearts", "spades", "clubs"];
+var deck = []
+
+function getDeck(){
+
+	for(var i = 0; i < suits.length; i++)
+	{
+		for(var x = 0; x < cards.length; x++)
+		{
+			var card = {Value: cards[x], Suit: suits[i]};
+			deck.push(card);
+		}
+	}
+
+	return deck;
+}
+
+function deal(){
+	console.log("hello")
+	// remove top card from deck
+	var card = deck[deck.length-1];
+	deck.splice(deck.length-1, 1);
+	return card;
+}
+
+function shuffle(){
+	
+	for (var i = 0; i < 1000; i++){
+		
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
+
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
+}
+
+function renderDeck(){
+	
+	for(var i = 0; i < deck.length; i++){
+		
+		var card = document.createElement("div");
+		var value = document.createElement("div");
+		var suit = document.createElement("div");
+		card.className = "card";
+		value.className = "value";
+		suit.className = "suit " + deck[i].Suit;
+
+		value.innerHTML = deck[i].Value;
+		card.appendChild(value);
+		card.appendChild(suit);
+
+		document.getElementById("deck").appendChild(card);
+	}
+}
